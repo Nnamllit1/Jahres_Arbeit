adcdafc29d110dd6a8c67e29b3856b364f313584
kuehnel.tillmann <Kuehnel.tillmann@SRZ.SRZ-DRESDEN.DE>
2024-01-31 17:54:32 +0100

    q

--- main_test.java
+++ main_test.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.table.*;
 
 /**
  *
@@ -13,14 +14,17 @@ import javax.swing.event.*;
 
 public class main_test extends JFrame {
   // Anfang Attribute
+  private JTable jTable1 = new JTable(5, 5);
+    private DefaultTableModel jTable1Model = (DefaultTableModel) jTable1.getModel();
+    private JScrollPane jTable1ScrollPane = new JScrollPane(jTable1);
   // Ende Attribute
   
   public main_test() { 
     // Frame-Initialisierung
     super();
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    int frameWidth = 300;
-    int frameHeight = 300;
+    int frameWidth = 500; 
+    int frameHeight = 500;
     setSize(frameWidth, frameHeight);
     Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
     int x = (d.width - getSize().width) / 2;
@@ -32,6 +36,13 @@ public class main_test extends JFrame {
     cp.setLayout(null);
     // Anfang Komponenten
     
+    jTable1ScrollPane.setBounds(0, 0, 300, 150);
+    jTable1.getColumnModel().getColumn(0).setHeaderValue("Title 1");
+    jTable1.getColumnModel().getColumn(1).setHeaderValue("Title 2");
+    jTable1.getColumnModel().getColumn(2).setHeaderValue("Title 3");
+    jTable1.getColumnModel().getColumn(3).setHeaderValue("Title 4");
+    jTable1.getColumnModel().getColumn(4).setHeaderValue("Title 5");
+    cp.add(jTable1ScrollPane);
     // Ende Komponenten
     
     setVisible(true);
