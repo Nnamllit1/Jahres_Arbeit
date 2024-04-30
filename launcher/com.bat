cd00aaca83403f99a1d013d67037becfb7d75ea8
Nnamllit1 <158311675+Nnamllit1@users.noreply.github.com>
2024-04-30 13:46:48 +0200

    Add files via upload

--- launcher/com.bat
+++ launcher/com.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Java\jdk-21\bin\jar.exe" -cfvm launcher.jar C:\Users\Tillmann\AppData\Local\Temp\MANIFEST.MF *.class JEClasses.jar json-java.jar
+"C:\Program Files\Java\jdk-21\bin\jar.exe" -cfvm launcher.jar "C:\Users\Tillmann\Desktop\Jahres Arbeit\Jahres_Arbeit-main\launcher\META-INF\MANIFEST.MF" *.class JEClasses.jar json-java.jar
 exit
--- launcher/launcher.java
+++ launcher/launcher.java
@@ -111,7 +111,38 @@ public class launcher extends JFrame {
   
   public void b_PlayUpdate_ActionPerformed(ActionEvent evt) {
     // TODO add your code here
-    
+            try {
+            // Pfad zum aktuellen Verzeichnis
+            String currentDir = System.getProperty("user.dir");
+
+            // Pfad zum übergeordneten Verzeichnis
+            String parentDir = Paths.get(currentDir).getParent().toString();
+
+            // Pfad zur JAR-Datei
+            String jarPath = parentDir + "/main.jar";
+      
+            System.out.println("java" + "-jar: " + jarPath + "\n");
+
+            // Erstellen Sie einen neuen Prozess
+            ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath);
+
+            // Starten Sie den Prozess
+            Process process = processBuilder.start();
+
+            // Lesen Sie die Ausgabe
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Warten Sie, bis der Prozess beendet ist
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
   } // end of b_PlayUpdate_ActionPerformed
 
   // end methods
